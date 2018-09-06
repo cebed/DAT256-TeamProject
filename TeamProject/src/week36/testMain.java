319139e328758e5a34546351a6138e81bfc48fb9
cebed <36077080+cebed@users.noreply.github.com>
2018-09-06 23:10:07 +0200

    Ready2go

--- TeamProject/src/week36/testMain.java
+++ TeamProject/src/week36/testMain.java
@@ -5,5 +5,7 @@ public class testMain {
 	public static void main(String [ ] args)
 	{
 System.out.println("Welcome To The Project");
+System.out.println("Upload teamreflection and individualfile to github");
+
 }
 }
--- TeamProject/src/week36/tested.java
+++
@@ -1,9 +0,0 @@
-package week36;
-
-public class tested {
-	
-	public static void main(String [ ] args)
-	{
-System.out.println("Upload teamreflection and individualfile to github");
-}
-}

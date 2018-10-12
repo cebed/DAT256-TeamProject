97e99b7c9a88865446772c1beb65c01ee709c134
nurhusein <nurabd@student.chalmers.se>
2018-10-12 11:17:13 +0200

    små-ändringar

--- app/src/main/java/com/semcon/oil/carpoc/QRActivity.java
+++ app/src/main/java/com/semcon/oil/carpoc/QRActivity.java
@@ -9,5 +9,8 @@ public class QRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

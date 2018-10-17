8da53b51652ca2c002b32a2eb711eb6102bd9973
cebed <36077080+cebed@users.noreply.github.com>
2018-10-17 12:51:18 +0200

    uppercase letter

--- app/src/main/java/com/semcon/oil/carpoc/database/Dataforstore.java
+++ app/src/main/java/com/semcon/oil/carpoc/database/Dataforstore.java
@@ -9,7 +9,7 @@ public class Dataforstore {
     private int [] Images = {R.drawable.kaffe,R.drawable.glass,R.drawable.tank,R.drawable.biltvatt,R.drawable.headset,R.drawable.spolar,R.drawable.motor,R.drawable.bio,R.drawable.iphone};
 
 
-    private String [] Names = {"kaffe","glass","tank","biltvätt","hörlurar","spolarvätska","Motorolja","biobiljett","iphone"};
+    private String [] Names = {"Kaffe","Glass","Bränsle","Biltvätt","Hörlurar","Spolarvätska","Motorolja","Biobiljett","Iphone"};
    //private int price  [] = {250,500,1000,1500,2500,3000,3500,5000,10000}; // orginal prices
 
     private int price  [] = {1,2,3,4,5,10,20,25,30}; // prices for demo

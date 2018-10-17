baaad013deb67dd85ab3be3a0b25685b7b4ffcb2
nurhusein <nurabd@student.chalmers.se>
2018-10-17 10:08:52 +0200

    ändrarde priser

--- app/src/main/java/com/semcon/oil/carpoc/database/Dataforstore.java
+++ app/src/main/java/com/semcon/oil/carpoc/database/Dataforstore.java
@@ -7,22 +7,16 @@ import com.semcon.oil.carpoc.R;
 public class Dataforstore {
 
     private int [] Images = {R.drawable.kaffe,R.drawable.glass,R.drawable.tank,R.drawable.biltvatt,R.drawable.headset,R.drawable.spolar,R.drawable.motor,R.drawable.bio,R.drawable.iphone};
-    private String [] Image_Strings ={"R.drawable.cherry","R.drawable.cola","R.drawable.fresquito","R.drawable.fruit_salad","R.drawable.cherry","R.drawable.cola","R.drawable.fresquito","R.drawable.fruit_salad","R.drawable.cherry","R.drawable.cola","R.drawable.fresquito"};
 
-    private String [] Names = {"cherry","cola","fresquito","fruit_salad","cherry","cola","fresquito","fruit_salad","cherry","cola","fresquito","fruit_salad"};
-    private int price  [] = {10,40,101,120,10,40,101,120,10,40,101,120};
+
+    private String [] Names = {"kaffe","glass","tank","biltvätt","hörlurar","spolarvätska","Motorolja","biobiljett","iphone"};
+    private int price  [] = {250,500,1000,1500,2500,3000,3500,5000,10000};
 
 
     public int getImages(int position) {
         return Images[position];
     }
-    public String get_Images_I_Strings(int position){
-        return Image_Strings[position];
-    }
 
-    public int Imagelength() {
-        return Images.length;
-    }
 
     public String getNames(int position) {
         return Names[position];

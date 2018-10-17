9da493b84662e819a7d17ee5337d73c51cdf9b29
cebed <36077080+cebed@users.noreply.github.com>
2018-10-17 12:52:53 +0200

    structure on code

--- app/src/main/java/com/semcon/oil/carpoc/database/Dataforstore.java
+++ app/src/main/java/com/semcon/oil/carpoc/database/Dataforstore.java
@@ -3,16 +3,16 @@ package com.semcon.oil.carpoc.database;
 import com.semcon.oil.carpoc.R;
 
 
-
 public class Dataforstore {
 
-    private int [] Images = {R.drawable.kaffe,R.drawable.glass,R.drawable.tank,R.drawable.biltvatt,R.drawable.headset,R.drawable.spolar,R.drawable.motor,R.drawable.bio,R.drawable.iphone};
+    private int[] Images = {R.drawable.kaffe, R.drawable.glass, R.drawable.tank, R.drawable.biltvatt, R.drawable.headset, R.drawable.spolar, R.drawable.motor, R.drawable.bio, R.drawable.iphone};
 
 
-    private String [] Names = {"Kaffe","Glass","Bränsle","Biltvätt","Hörlurar","Spolarvätska","Motorolja","Biobiljett","Iphone"};
-   //private int price  [] = {250,500,1000,1500,2500,3000,3500,5000,10000}; // orginal prices
+    private String[] Names = {"Kaffe", "Glass", "Bränsle", "Biltvätt", "Hörlurar", "Spolarvätska", "Motorolja", "Biobiljett", "Iphone"};
+    //private int price  [] = {250,500,1000,1500,2500,3000,3500,5000,10000}; // orginal prices
 
-    private int price  [] = {1,2,3,4,5,10,20,25,30}; // prices for demo
+    private int price[] = {1, 2, 3, 4, 5, 10, 20, 25, 30}; // prices for demo
+
     public int getImages(int position) {
         return Images[position];
     }
@@ -27,9 +27,6 @@ public class Dataforstore {
 
         return price[position];
     }
-
-
-
 
 
 }
